1efdef4ea9a30a1571bc9a739d9b9f26a8fa3f90
Dungweb <116371263+Dungweb@users.noreply.github.com>
2023-03-17 00:39:05 +0700

    nn

--- src/com/cafe/GUI/LoginGUI.java
+++ src/com/cafe/GUI/LoginGUI.java
@@ -7,43 +7,96 @@ import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import java.awt.Color;
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
 
     public class LoginGUI extends javax.swing.JFrame {
         public LoginGUI() {
+            initComponents();
             setBackground(new Color(0,0,0,0));
             setVisible(true);
         }
 
-        @SuppressWarnings("unchecked")
-        // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+        private void initComponents() {
 
-        private void jTextField6FocusGained(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_jTextField6FocusGained
-            if(jTextField6.getText().equals("Tên đăng nhập")){
-                jTextField6.setText("");
-                jTextField6.setForeground( new Color(255,255,255));
-            }
-        }//GEN-LAST:event_jTextField6FocusGained
+            roundPanel1 = new com.cafe.Custom.RoundPanel();
+            jPanel1 = new javax.swing.JPanel();
+            jLabel2 = new javax.swing.JLabel();
+            jPanel2 = new javax.swing.JPanel();
+            jTextField6 = new javax.swing.JTextField();
+            jPasswordField1 = new javax.swing.JPasswordField();
+            button1 = new com.cafe.Custom.Button();
+            button2 = new com.cafe.Custom.Button();
+            button3 = new com.cafe.Custom.Button();
 
-        private void jTextField6FocusLost(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_jTextField6FocusLost
-            if(jTextField6.getText().equals("")){
-                jTextField6.setText("Tên đăng nhập");
-                jTextField6.setForeground( new Color(153,153,153));
-            }
-        }//GEN-LAST:event_jTextField6FocusLost
+            setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+            setBackground(new java.awt.Color(186, 255, 116));
+            setLocation(new java.awt.Point(400, 200));
+            setUndecorated(true);
 
-        private void jPasswordField1FocusGained(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_jPasswordField1FocusGained
-            if(jPasswordField1.getText().equals("password")){
-                jPasswordField1.setText("");
-                jPasswordField1.setForeground( new Color(255,255,255));
-            }
-        }//GEN-LAST:event_jPasswordField1FocusGained
+            roundPanel1.setBackground(new java.awt.Color(0, 0, 0));
 
-        private void jPasswordField1FocusLost(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_jPasswordField1FocusLost
-            if(jPasswordField1.getText().equals("")){
-                jPasswordField1.setText("password");
-                jPasswordField1.setForeground( new Color(153,153,153));
-            }
-        }//GEN-LAST:event_jPasswordField1FocusLost
+
+            jTextField6.setBackground(new java.awt.Color(0, 0, 0));
+            jTextField6.setFont(new java.awt.Font("Times New Roman", 0, 14)); // NOI18N
+            jTextField6.setForeground(new java.awt.Color(153, 153, 153));
+            jTextField6.setText("Tên đăng nhập");
+            jTextField6.setAlignmentX(1.0F);
+
+            jPasswordField1.setBackground(new java.awt.Color(0, 0, 0));
+            jPasswordField1.setFont(new java.awt.Font("Times New Roman", 0, 14)); // NOI18N
+            jPasswordField1.setForeground(new java.awt.Color(153, 153, 153));
+            jPasswordField1.setText("password");
+            jPasswordField1.addFocusListener(new java.awt.event.FocusAdapter() {
+
+
+            });
+
+            button1.setBorder(null);
+            button1.setText("X");
+            button1.setFocusPainted(false);
+            button1.setFont(new java.awt.Font("Times New Roman", 0, 14)); // NOI18N
+            button1.setRadius(15);
+            button1.addMouseListener(new java.awt.event.MouseAdapter() {
+                public void mouseClicked(java.awt.event.MouseEvent evt) {
+                    button1MouseClicked(evt);
+                }
+            });
+
+
+            button2.setBorder(null);
+            button2.setText("-");
+            button2.setFocusPainted(false);
+            button2.setFont(new java.awt.Font("Times New Roman", 0, 14)); // NOI18N
+            button2.setMaximumSize(new java.awt.Dimension(43, 25));
+            button2.setMinimumSize(new java.awt.Dimension(43, 25));
+            button2.setRadius(15);
+            button2.addMouseListener(new java.awt.event.MouseAdapter() {
+                public void mouseClicked(java.awt.event.MouseEvent evt) {
+                    button2MouseClicked(evt);
+                }
+            });
+
+
+
+            button3.setBackground(new java.awt.Color(250, 110, 240));
+            button3.setBorder(null);
+            button3.setText("Đăng nhập");
+            button3.setFocusPainted(false);
+            button3.setFont(new java.awt.Font("Times New Roman", 0, 14)); // NOI18N
+            button3.setRadius(10);
+            button3.addMouseListener(new java.awt.event.MouseAdapter() {
+                public void mouseClicked(java.awt.event.MouseEvent evt) {
+                    button3MouseClicked(evt);
+                }
+            });
+
+        }
 
         private void button1MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_button1MouseClicked
             System.exit(0);
@@ -53,15 +106,14 @@ import java.util.logging.Logger;
             setState(LoginGUI.ICONIFIED);
         }//GEN-LAST:event_button2MouseClicked
 
-        private void jPanel1MousePressed(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jPanel1MousePressed
-            mouseX = evt.getX();
-            mouseY = evt.getY();
-        }//GEN-LAST:event_jPanel1MousePressed
+        private void button3MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_button3MouseClicked
+            if(jTextField6.getText().equals("") || jPasswordField1.getText().equals("")) {
+                JOptionPane.showMessageDialog(this, "Vui lòng không để trống!", "Thông báo", JOptionPane.INFORMATION_MESSAGE);
+            }
+        }
 
-        private void jPanel1MouseDragged(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jPanel1MouseDragged
-            super.setLocation(super.getX() + evt.getX() - mouseX, super.getY() + evt.getY() - mouseY);
-        }//GEN-LAST:event_jPanel1MouseDragged
-
+        @SuppressWarnings("unchecked")
+        // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
 
 
         public static void main(String args[]) {
@@ -74,12 +126,15 @@ import java.util.logging.Logger;
 
         // Variables declaration - do not modify//GEN-BEGIN:variables
 
+        private com.cafe.Custom.Button button1;
+        private com.cafe.Custom.Button button2;
+        private com.cafe.Custom.Button button3;
         private javax.swing.JLabel jLabel2;
         private javax.swing.JPanel jPanel1;
         private javax.swing.JPanel jPanel2;
         private javax.swing.JPasswordField jPasswordField1;
         private javax.swing.JTextField jTextField6;
-
+        private com.cafe.Custom.RoundPanel roundPanel1;
         // End of variables declaration//GEN-END:variables
         private int mouseX,mouseY;
     }
